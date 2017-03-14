8d38636603ffee7fd8a60962d5416877a2e061b0
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-03-14 20:03:36 +0100

    Link Github

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/jindrichmynarz/sparclj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/jindrichmynarz/sparclj"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/data.zip "0.1.2"]
                  [org.clojure/data.xml "0.0.8"]
