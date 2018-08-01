454f7922d287d9cea0c01681f145f464501b9337
Jindřich Mynarz <mynarzjindrich@gmail.com>
2018-08-01 19:08:32 +0200

    Upgrade clj-http

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sparclj "0.2.2"
+(defproject sparclj "0.2.3"
   :description "A Clojure library for talking with SPARQL endpoints"
   :url "https://github.com/jindrichmynarz/sparclj"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.zip "0.1.2"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
-                 [clj-http "3.7.0"]
+                 [clj-http "3.9.0"]
                  [slingshot "0.12.2"]
                  [commons-validator/commons-validator "1.6"]
                  [stencil "0.5.0"]]
